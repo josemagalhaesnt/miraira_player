59e097b57011125119a9db8af42f5331198dbb49
Zéh Neto <zneto@live.com>
2018-06-18 12:34:08 -0300

    v. 1.6.0
    Adicionado menu de rodapé

--- inc/player_footer.php
+++ inc/player_footer.php
@@ -2,5 +2,39 @@
 </div>
 </div>
 </section>
+
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div id="menu-rodape">
+					<div class="btn-group d-none d-lg-block d-xl-block">
+    					<a class="btn btn-outline-light" href="http://www.digitalmundomiraira.com.br/" role="button">
+  								<i class="fas fa-home"></i> Voltar para a página inicial 
+  							</a>
+
+  							<a class="btn btn-outline-light" href="javascript:window.history.go(-1)" role="button">
+  								<i class="fas fa-undo"></i> Voltar para a página anterior
+  							</a>
+					</div>
+
+					<div class="btn-group-vertical d-lg-none">
+    					<a class="btn btn-outline-light" href="http://www.digitalmundomiraira.com.br/" role="button">
+  								<i class="fas fa-home"></i> Voltar para a página inicial 
+  							</a>
+
+  							<a class="btn btn-outline-light" href="javascript:window.history.go(-1)" role="button">
+  								<i class="fas fa-undo"></i> Voltar para a página anterior
+  							</a>
+					</div>
+  							
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+
 </body>
 </html>
--- inc/player_header.php
+++ inc/player_header.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans|Ubuntu" rel="stylesheet">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 	<link rel="stylesheet" href="/miraira_player/css/player.css">
 	<title>Player de Mídia</title>
 </head>
