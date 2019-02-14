211462ad1ba6be4dbda8a76ea889e2ef4d6b1779
Zéh Neto <zneto@live.com>
2019-02-14 12:28:14 -0300

    adicionada constante auth_download

--- config.php
+++ config.php
@@ -2,10 +2,13 @@
 
   session_start();
 
+  //TEMPLATE
+
   define("PLAYER_HEADER", '/miraira_player/inc/player_header.php');
   define("PLAYER_FUNCTIONS", '/miraira_player/inc/player_functions.php');
   define("MUSIC_PLAYER", '/miraira_player/inc/audio_player_box.php');
   define("VIDEO_PLAYER", '/miraira_player/inc/video_player_box.php');
   define("PLAYER_FOOTER", '/miraira_player/inc/player_footer.php');
+  define("DOWNLOAD_AUTH", '/miraira_player/inc/download.php');
 
 ?>
