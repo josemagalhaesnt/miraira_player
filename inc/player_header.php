3e9b1cec1390eab200f01856dc264b9cbb56b3aa
Zéh Neto <zneto@live.com>
2019-02-15 17:03:34 -0300

    2.0.4
    -Adicionado favicon

--- inc/player_header.php
+++ inc/player_header.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link rel="icon" href="https://i1.wp.com/www.digitalmundomiraira.com.br/wp-content/uploads/logos/cropped-icon.png?fit=32%2C32" sizes="32x32">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 	<link rel="stylesheet" href="/miraira_player/css/player.css">
